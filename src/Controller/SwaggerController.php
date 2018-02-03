15197d487019d85cc98c235c1ba9ba30f84e23bb
Olla Tech <olla.code@gmail.com>
2018-02-03 23:21:39 +0700

    initial

---
+++ src/Controller/SwaggerController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Olla\Devtool\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Twig\Environment as TwigEnvironment;
+
+final class SwaggerController
+{
+    /**
+     * @var TwigEnvironment
+     */
+    private $twig;
+
+    public function __construct() {
+        $this->twig = $twig;
+    }
+
+    public function indexAction($schemaName = null)
+    {
+        return Response::create($this->twig->render($this->viewConfig->getTemplate(),
+            [
+                'endpoint' => $endpoint,
+                'versions' => [
+                    'graphiql' => $this->viewConfig->getJavaScriptLibraries()->getGraphiQLVersion(),
+                    'react' => $this->viewConfig->getJavaScriptLibraries()->getReactVersion(),
+                    'fetch' => $this->viewConfig->getJavaScriptLibraries()->getFetchVersion(),
+                ],
+            ]
+        ));
+    }
+}
